df4f348740f36d847906bc73edf077e87f7b6c0f
sanjel <arun_sanjel1@baylor.edu>
2019-07-23 15:13:39 -0500

    Added ARIA alerts and CSS fixes

--- blog.js
+++ blog.js
@@ -1,5 +1,15 @@
 $("#comment_post").click(function(){
+	var name = $("name").val();
+	var comment = $("user_comment").val();
+	if (name == "" || comment == ""){
+		$("#message-body").text("Please Valid Name or Comment! ");
+	}
+	else{
+		$("#message-body").text("Comment Uploaded!")
+	}
 	document.getElementById("message").className = "";
 });
-
+$("#btn-close").click(function(){
+	document.getElementById("message").className = "alert-message";
+});
 
