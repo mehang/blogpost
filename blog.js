0058587fdb135f570b2805d321702e6341b927c0
sanjel <arun_sanjel1@baylor.edu>
2019-07-23 16:40:49 -0500

    All error Fixed

--- blog.js
+++ blog.js
@@ -1,13 +1,13 @@
 $("#comment_post").click(function(){
 	var name = $("#name").val();
-	var comment = $("#user_comment").val();
-	
-	if (name == "" || comment == ""){
+	var comment = $("textarea#user_comment").val();
+	console.log(comment)
+	if ((name == "" )||( comment == "")){
 		if (name == ""){ 
 		$("#message-body").text("Please Provide Valid Name!!");
 		}
-		else{
-		$("#message-body").text("Please Provide Valid Name!!");
+		else {
+		$("#message-body").text("Please Provide Valid Comment!!");
 		}
 	}
 	else{
